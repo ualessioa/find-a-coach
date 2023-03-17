e6e34400f4a82f4aac7d90e18682494c1006f7c6
frankhood <frankhood@iMacLate2015.lan>
2023-03-17 16:51:25 +0100

    lecture 244

--- src/Pages/coach/CoachCreate.vue
+++ src/Pages/coach/CoachCreate.vue
@@ -1,1 +1,16 @@
-<template>CoachCreate</template>
+<template>
+  <base-card>
+    <section>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </section>
+  </base-card>
+</template>
+
+<script>
+import CoachForm from '../../components/coach/CoachForm.vue';
+
+export default {
+  components: { CoachForm },
+};
+</script>
